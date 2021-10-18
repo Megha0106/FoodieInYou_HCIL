a249b952f56625fe3f5ace69e52e6dcaa8cc77a9
Smeet <smeetramteke21@gmail.com>
2021-10-18 11:21:27 +0530

    Commit 1: Basic File Structure Created

---
+++ src/Pages/Read/Read.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Read.css'
+
+function Read() {
+    return (
+        <div>
+            Read
+        </div>
+    )
+}
+
+export default Read
---
+++ src/Pages/Upload/Upload.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Upload.css'
+
+function Upload() {
+    return (
+        <div>
+            Upload
+        </div>
+    )
+}
+
+export default Upload
